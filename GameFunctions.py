59d38832410d519bd47972887efed7d326007de8
kaengurubeutel <68818935+kaengurubeutel@users.noreply.github.com>
2022-03-18 11:59:55 +0100

    Update GameFunctions.py
    added Documentation

--- GameFunctions.py
+++ GameFunctions.py
@@ -1,15 +1,29 @@
 import random
 
 
-# deal a Card
 def deal_card(player: list, all_cards: list):
+    """
+    This function takes in a list of cards and returns a string of the cards in the list
+    
+    :param player: list
+    :type player: list
+    :return: a string.
+    """
     card = random.choice(all_cards)
     player.append(card)
     all_cards.remove(card)
 
 
-# returns total amount of the player that's on the turn as INT
+
 def total(player: list) -> int:
+    """
+    The function `total` takes a list of cards as an argument and returns the sum of the values of those
+    cards
+    
+    :param player: list
+    :type player: list
+    :return: The total value of the cards in the player's hand.
+    """
     total = 0
     for card in player:
         total += card.value
@@ -17,8 +31,14 @@ def total(player: list) -> int:
     return total
 
 
-# show 1/2 cards as the amount of cards increases
 def reveal_dealer_hand(dealerHand: list):
+    """
+    Given a list of cards in the dealer's hand, return a list of the cards that are face up
+    
+    :param dealerHand: list of cards in the dealer's hand
+    :type dealerHand: list
+    :return: The first two cards in the dealer's hand.
+    """
     out = []
     if len(dealerHand) == 2:
         out.append(dealerHand[0])
@@ -28,11 +48,18 @@ def reveal_dealer_hand(dealerHand: list):
     return out
 
 
-# returns own hand as string
 def print_hand(Hand: list):
+    """
+    Prints the cards in a hand
+    
+    :param Hand: a list of Card objects
+    :type Hand: list
+    :return: A string of the cards in the hand.
+    """
     out = ""
 
     for card in Hand:
         out += "" + card.to_string()
         out += "\n "
     return out
+
